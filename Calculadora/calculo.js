9744cc2e54c9f49aded12053442c712f83475f73
gabigringavolei <130366455+gabigringavolei@users.noreply.github.com>
2023-09-25 11:41:57 -0300

    primeiro envio

---
+++ Calculadora/calculo.js
@@ -0,0 +1,6 @@
+let nota1 = document.querySelector(".nota1").textContent;
+console.log(nota1);
+let nota2 = document.querySelector(".nota2").textContent;
+console.log(nota2);
+let nota3 = document.querySelector(".nota3").textContent;
+console.log(nota3);
